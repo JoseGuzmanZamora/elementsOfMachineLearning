664d3fe24069f8274163f00a68d8832259c87e72
José Guzmán <joseguzmanzamora@gmail.com>
2020-01-14 09:24:35 -0600

    Refinar problema 23

---
+++ ejerciciosPython/problema23/main.py
@@ -0,0 +1,4 @@
+import module as m
+
+archivo = m.abrir_archivo("triangle.txt")
+print(m.recorrido(archivo))
--- ejerciciosPython/problema23/module.py
+++ ejerciciosPython/problema23/module.py
@@ -1,25 +1,34 @@
-# José Alejandro Guzmán Zamora 
+"""
+Largest Sum Module.
 
-'''
-Instrucciones 
-Escriba un programa que lea los contenidos de triangle.txt
-, y calcule el camino de adyacentes que suma el total más largo.
-'''
+Functions:
+    abrir_archivo -- opens file from directory 
+    recorrido -- finds the largest sum path in a triangle
+"""
 
-archivo = open("triangle.txt","r")
+def abrir_archivo(name):
+    """Open file from name and separate by whitespace."""
+    with open(name) as file:
+        data = file.read().split('\n')
+    return [i.split(' ') for i in data]
 
-info = archivo.read().split('\n')
+def recorrido(info):
+    """
+    Bottom up path through the triangle. 
+    
+    It starts finding the largest number from each pair in each row 
+    and then adds it to its corresponding father until reaching the 
+    top.
+    
+    Arguments:
+        info -- Matrix that represents the triangle
+    Returns:
+        Largest sum from path
+    """
+    for i in range(len(info) - 1, -1, -1):
+        for j in range(len(info[i]) - 1):
+            mayor = max(info[i][j], info[i][j + 1])
+            info[i - 1][j] = int(info[i - 1][j]) + int(mayor)
+    return info[0]
 
-for i in range(len(info)):
-    info[i] = info[i].split(' ')
 
-# Ya tenemos la informacion ahora hay que recorrer de abajo hacia arriba 
-
-for i in range(len(info) - 1, -1, -1):
-    '''for j in range(len(info[i]) - 1):
-        mayor = max(info[i][j], info[i][j + 1])
-        info[i - 1][j] += mayor'''
-    for j in range(len(info[i]) - 1):
-        mayor = max(info[i][j], info[i][j + 1])
-        info[i - 1][j] = int(info[i - 1][j]) + int(mayor)
-    print(info[i])
