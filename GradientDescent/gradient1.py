7308b438e84138fdca9766044cf5e21c4634c30d
José Guzmán <joseguzmanzamora@gmail.com>
2020-01-29 15:57:09 -0600

    modificaciones adicionales, comentarios

--- GradientDescent/gradient1.py
+++ GradientDescent/gradient1.py
@@ -1,7 +1,19 @@
-# First Version of Gradient Descent specifically made for Linear Regression 
-import numpy as np 
+# José Alejandro Guzmán Zamora
+
 
 def actualizacion(train,theta0,theta1,alpha,step):
+    """
+    Aplicación de derivadas parciales a theta 1 y theta 2. 
+
+    Params:
+        - train : dataset de entrenamiento 
+        - theta0 : valor actual de theta 0 
+        - theta1 : valor actual de theta 1 
+        - alpha : learning rate 
+        - step : True para modificar theta0, False para theta1
+    Returns:
+        - Resultado de operación
+    """
     m = len(train)
     if step:
         lista = [theta0 + (theta1 * train[i][0]) 
@@ -12,6 +24,16 @@ def actualizacion(train,theta0,theta1,alpha,step):
     return (alpha / m) * sum(lista)
 
 def gradient_descent(train, alpha, iteraciones):
+    """
+    Aplica Gradient Descent sobre un dataset de entrenamiento. 
+
+    Params:
+        - train : valores x y y para entrenamiento 
+        - alpha : training rate 
+        - iteraciones : indica cuántas iteraciones realizar 
+    Returns:
+        - Theta 0 y theta 1 finales 
+    """
     zeta = [0,0]
     for i in range(iteraciones):
         temp0 = zeta[0] - actualizacion(train, zeta[0], zeta[1], alpha, True)
@@ -20,7 +42,7 @@ def gradient_descent(train, alpha, iteraciones):
         zeta[1] = temp1
     return zeta
 
-
+# Prueba 
 dataset = [[2,5],[3,6],[4,9],[5,9],[9,12]]
-alpha = 1.5
+alpha = 0.5
 print(gradient_descent(dataset, alpha, 100))
