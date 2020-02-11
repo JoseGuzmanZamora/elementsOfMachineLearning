9cc8b7d24d939b4433ba513a2e08effec922fc91
José Guzmán <joseguzmanzamora@gmail.com>
2020-02-10 23:49:52 -0600

    Gradient Descent bueno, vectorizado y funcional

--- GradientDescent/gradient3.py
+++ GradientDescent/gradient3.py
@@ -1,33 +1,43 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-# FUNCION DE COSTO 
-'''
-Ingresa un X con una fila de 1s para hacer la multiplicación en el futuro con Xi 
-Entra un Y con los valores que tenemos como predicción 
-
-J(z0,z1) = 1 / 2m * sumatoria de 1 a m (donde m es la cantidad de datos) 
-
-(H(xi) - Yi) ^ 2 
-
-Esta definición indica que tiene que ingresar un arreglo de tetas 
-'''
-
 def cost(X,Y,thetas):
+    """ 
+    Calcular el costo de un conjunto de hipótesis basado en 
+    la cantidad de thetas ingresados.
+    """
     hipos = np.matmul(X, thetas)
     return np.sum(np.power(hipos - Y, 2)) / (2 * X.shape[0])
 
-# FUNCION DE DERIVADA 
-
-'''
-Esta se puede expresar de la siguiente manera:
-dJ(theta)/dtheta0 = 1/m * sum de 1 a m de (H(thetas) - Yi) * Xi
- '''
-
 def derivative(X,Y,thetas):
+    """
+    Encontrar la derivada de la función de costo y retornar 
+    la suma de todas las hipótesis.
+    """
     hips = np.matmul(X, thetas)
     interno = (hips - Y).transpose()
     return (np.matmul(interno, X) / X.shape[0]).transpose()
+
+def gradient_descent(X, Y, thetas, a, it, delta):
+    """
+    Aplica descenso del gradiente en base a un dataset y thetas iniciales.
+
+    Params:
+    - X : valores de x con columna de 1s agregada al inicio
+    - Y : valores de entrenamiento
+    - a : learning rate
+    - it : cantidad de iteraciones
+    - delta : cambio mínimo para parar las iteraciones
+    """
+    procesado = np.asarray(thetas, dtype='float64').reshape(len(thetas),1)
+    for i in range(it):
+        costo1 = np.round(cost(X,Y,procesado),4)
+        procesado -= a * derivative(X,Y,procesado)
+        costo = np.round(cost(X,Y,procesado),4)
+        if costo1 - costo <= delta:
+            print(i)
+            break
+    return procesado
 
 
 # RANDOM DATASET 
@@ -39,10 +49,16 @@ ys = np.asarray([(0.6 * i + (3 * np.random.randint(10))) for i in x]).reshape(10
 xs = np.hstack((np.ones(100).reshape(100,1), x))
 
 thetas = [9,4]
+res = gradient_descent(xs, ys, thetas, 0.00001, 1000, 0.0005)
+print(res)
 
-procesado = np.asarray(thetas).reshape(len(thetas),1)
-print(cost(xs,ys,procesado))
-print(derivative(xs,ys,procesado))
+plt.scatter(x,ys)
+x = np.linspace(0,250,100)
+y = res[1] * x + res[0]
+plt.plot(x, y, '-r')
+plt.show()
+
+
 
 
 
