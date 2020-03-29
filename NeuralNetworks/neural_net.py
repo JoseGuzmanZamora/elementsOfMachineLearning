357c920bc3f1d82f2c7e360875457f8cb6163ca7
José Guzmán <joseguzmanzamora@gmail.com>
2020-03-29 16:43:54 -0600

    En teoría ya está listo back prop!!

--- NeuralNetworks/neural_net.py
+++ NeuralNetworks/neural_net.py
@@ -4,9 +4,9 @@ import copy
 def sigmoid(values):
     return 1 / (1 + np.exp(-values))
 
-def forward_setup(nodos, X):
+def forward_setup(nodos,X,Y):
     nodos.insert(0,X.shape[1])
-    nodos.append(1)
+    nodos.append(len(Y[0]))
     nodes = [i + 1 for i in nodos]
 
     # red, mesh de thetas 
@@ -20,7 +20,7 @@ def forward_setup(nodos, X):
     return thetas
 
 def forward_prop(X,thetas):
-    inicial = X.T
+    inicial = X.T      
     trace = [inicial]
     for i in range(len(thetas)):
         interno = np.matmul(
@@ -33,7 +33,7 @@ def forward_prop(X,thetas):
         trace.append(sigmoid(interno))
     return trace 
 
-def backward_prop(X,Y,thetas):
+def backward_prop(thetas,X,Y):
     m,_ = X.shape
     delta = copy.deepcopy(thetas)
     for i in delta: i[:] = 0
@@ -47,12 +47,18 @@ def backward_prop(X,Y,thetas):
             np.multiply(activation_trace[i],(1-activation_trace[i]))
             )
         deltas.append(first_delta)
+    deltas.append(None)
 
     deltas = list(reversed(deltas))
     for i in range(len(delta)):
-        print("Aim: ",delta[i].T.shape)
-        print(activation_trace[i + 1].shape)
-        print(deltas[i].T.shape)
+        temp_shape = activation_trace[i].shape
+        multi_res = np.matmul(
+            deltas[i + 1],
+            np.vstack([np.ones((1,temp_shape[1])),activation_trace[i]]).T
+            )
+        delta[i] += multi_res / m
+    return delta 
+    
 
 
 # DATASET FICTICIO
@@ -63,10 +69,6 @@ def random_variable(n):
 
 x_1 = random_variable(valores)
 x_2 = random_variable(valores)
-'''ones = np.expand_dims(
-    np.asarray([1 for i in range(valores)]),
-    1
-)'''
 
 # aqui ya tenemos el formato esperado de las equises, como columnas  
 xes = np.hstack([x_1,x_2])
@@ -74,12 +76,13 @@ xes = np.hstack([x_1,x_2])
 # aqui ya tenemos el formato esperado de las yes, como columnas tmb
 y = [0 if x_1[i] > 50 else 1 for i in range(valores)]
 y = np.expand_dims(np.asarray(y), 1)
+new_y = np.expand_dims(np.asarray([np.random.randint(0,3) for i in range(valores)]),1)
+introduce_y = (new_y == np.arange(3)).astype(int)
 
 # PARAMETERS SETUP 
-hidden_layers = 8
-nodes_inicio = [3,3,3,6,5,2,8,9]
+hidden_layers = 2
+nodes_inicio = [3,3]
 
 # LLAMADA A LA FUNCION
-thetas = forward_setup(nodes_inicio, xes)
-activations = forward_prop(xes,thetas)
-backward_prop(xes,y, thetas)
+thetas = forward_setup(nodes_inicio, xes,introduce_y)
+print(backward_prop(thetas,xes,introduce_y))
