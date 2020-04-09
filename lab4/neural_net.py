a03fb1532eac3980b0dd3d821279ecdc4ac55572
José Guzmán <joseguzmanzamora@gmail.com>
2020-04-08 23:26:57 -0600

    formalizando laboratorio 4

---
+++ lab4/neural_net.py
@@ -0,0 +1,83 @@
+import numpy as np 
+import copy 
+
+def sigmoid(values):
+    return 1 / (1 + np.exp(-values))
+
+def forward_setup(nodos,X,Y,hidden_layers):
+    nodos.insert(0,X.shape[1])
+    nodos.append(len(Y[0]))
+    nodes = [i + 1 for i in nodos]
+
+    # red, mesh de thetas 
+    thetas = []
+    # por el momento le voy a poner mas 1 por la output layer 
+    for i in range(hidden_layers + 1):
+        temp_primero = np.expand_dims(np.asarray([np.random.uniform() for i in range(nodes[i])],dtype='float64'),1)
+        thetas.append(np.matrix(np.asarray(
+            [temp_primero for i in range(nodos[i + 1])]
+            )))
+    return thetas
+
+def forward_prop(X,thetas):
+    inicial = X.T      
+    trace = [inicial]
+    for i in range(len(thetas)):
+        interno = np.matmul(
+            thetas[i],
+            np.vstack(
+                [np.expand_dims(np.ones(trace[i].shape[1]),0),
+                trace[i]]
+                )
+        )
+        trace.append(sigmoid(interno))
+    return trace 
+
+def backward_prop(thetas,shapes,X,Y):
+    m,_ = X.shape
+    # regresar a su normalidad 
+    thetas = unflatten_zetas(thetas,shapes)
+    delta = copy.deepcopy(thetas)
+    for i in delta: i[:] = 0
+    activation_trace = forward_prop(X,thetas)
+    first_delta = activation_trace[-1] - Y.T
+    deltas = [first_delta]
+    # backward prop 
+    for i in reversed(range(1,len(thetas))):
+        first_delta = np.multiply(
+            np.matmul(thetas[i].T[1:,:],first_delta),
+            np.multiply(activation_trace[i],(1-activation_trace[i]))
+            )
+        deltas.append(first_delta)
+    deltas.append(None)
+
+    deltas = list(reversed(deltas))
+    for i in range(len(delta)):
+        temp_shape = activation_trace[i].shape
+        multi_res = np.matmul(
+            deltas[i + 1],
+            np.vstack([np.ones((1,temp_shape[1])),activation_trace[i]]).T
+            )
+        delta[i] += multi_res / m
+    return flatten_zetas(delta)[0]
+    
+def flatten_zetas(thetas):
+    flat_zetas = np.asarray([])
+    shapes = []
+    for i in thetas:
+        shapes.append(i.shape)
+        flat_zetas = np.concatenate((flat_zetas,np.ravel(i)))
+    return (flat_zetas,shapes)
+
+def unflatten_zetas(flat_thetas,shapes):
+    res = []
+    for i in shapes:
+        cantidad = i[0] * i[1]  
+        res.append(flat_thetas[:cantidad].reshape(i))
+    return res 
+
+def cost(thetas,shapes,X,Y):
+    thetas = unflatten_zetas(thetas,shapes)
+    h = forward_prop(X,thetas)[-1]
+    calculate = (Y * np.log(h).T) + ((1 - Y) * np.log(1 - h).T)
+    return np.sum(calculate) / (- X.shape[0])
